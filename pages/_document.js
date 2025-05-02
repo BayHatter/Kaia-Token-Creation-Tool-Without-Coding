04d100f9b38a6bdf230a566331200f01b7d04a53
Mohsin Khan <209888552+BayHatter@users.noreply.github.com>
2025-05-02 18:20:29 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Ethereum Token Creation Tool</title>
-        <meta name="description" content="Create and launch your own meme token on Ethereum" />
-        <meta property="og:title" content="Launch Your Meme Coin on Ethereum" />
+        <title>Kaia Mainnet Token Creation Tool</title>
+        <meta name="description" content="Create and launch your own meme token on Kaia Mainnet" />
+        <meta property="og:title" content="Launch Your Meme Coin on Kaia Mainnet" />
         <meta property="og:description" content="Build and deploy a meme coin instantly. Fast, secure." />
         <meta property="og:url" content="/" />
         <meta property="og:type" content="website" />
